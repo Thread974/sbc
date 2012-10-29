e05013af0485dd400dce4105be9b872e861bab47
Frédéric Dalleau <frederic.dalleau@linux.intel.com>
2012-10-29 10:03:29 +0100

    sbc: Rename sbc_analyze_4b_xx to sbc_analyze_xx

--- sbc/sbc.c
+++ sbc/sbc.c
@@ -691,7 +691,7 @@ static int sbc_analyze_audio(struct sbc_encoder_state *state,
 			x = &state->X[ch][state->position - 4 * state->increment +
 							frame->blocks * 4];
 			for (blk = 0; blk < frame->blocks; blk += state->increment) {
-				state->sbc_analyze_4b_4s(
+				state->sbc_analyze_4s(
 					state, x,
 					frame->sb_sample_f[blk][ch],
 					frame->sb_sample_f[blk + 1][ch] -
@@ -706,7 +706,7 @@ static int sbc_analyze_audio(struct sbc_encoder_state *state,
 			x = &state->X[ch][state->position - 8 * state->increment +
 							frame->blocks * 8];
 			for (blk = 0; blk < frame->blocks; blk += state->increment) {
-				state->sbc_analyze_4b_8s(
+				state->sbc_analyze_8s(
 					state, x,
 					frame->sb_sample_f[blk][ch],
 					frame->sb_sample_f[blk + 1][ch] -
